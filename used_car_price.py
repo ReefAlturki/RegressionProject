32de1c81e1249aef7c231ad82d6da834c10f0acc
ReefAlturki <76409162+ReefAlturki@users.noreply.github.com>
2021-12-25 23:32:02 +0300

    Add files via upload

--- used_car_price.py
+++ used_car_price.py
@@ -1,21 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
-from sklearn.datasets import load_boston
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+
 import pickle
 
 #title
 st.title('Used car price prediction')
 list_of_states= ('CA','FL','TX','NY','None of the above')
 state=st.selectbox('Select The state:', list_of_states)
-list_of_manf= ('Acura', 'Alfa', 'Alfa-Romeo','Aston', 'Aston-Martin', 'Audi', 'Bentley', 'Bmw', 'Buick','Cadillac', 'Chevrolet', 'Chrysler', 'm.Datsun', 'm.Dodge',
+list_of_manf= ('Acura', 'Alfa', 'Alfa-Romeo','Aston', 'Aston-Martin', 'Audi', 'Bentley', 'Bmw', 'Buick','Cadillac', 'Chevrolet', 'Chrysler', 'Datsun', 'Dodge',
        'Ferrari', 'Fiat', 'Ford', 'Genesis', 'Gmc','Harley-Davidson', 'Honda', 'Hummer', 'Hyundai', 'Infiniti',
        'Jaguar', 'Jeep', 'Karma', 'Kia', 'Lamborghini', 'Land','Land Rover', 'Lexus', 'Lincoln', 'Maserati', 'Mazda',
        'Mclaren', 'Mercedes-Benz', 'Mercury', 'Mini', 'Mitsubishi','Morgan', 'Nissan', 'Not specified', 'Pontiac', 'Porsche',
@@ -23,16 +16,16 @@ list_of_manf= ('Acura', 'Alfa', 'Alfa-Romeo','Aston', 'Aston-Martin', 'Audi', 'B
        'Volvo')
 manf = st.selectbox('Select The Manufacturer of your car:',list_of_manf )
 #tr
-list_of_tran= ('t.Automatic', 't.CVT','t.Manual')
+list_of_tran= ('Automatic', 'CVT','Manual','None')
 Transmission= st.selectbox('Select The type of transmission on your car:', list_of_tran)
 #feul
-list_of_feul= ('fuel.Diesel', 'fuel.Electric', 'fuel.Flexible',
-       'fuel.Gasoline', 'fuel.Hybrid', 'fuel.Other')
+list_of_feul= ('Diesel', 'Electric', 'Flexible',
+       'Gasoline', 'Hybrid', 'Other')
 feul= st.selectbox('Select The Fuel type on your car:', list_of_feul)
 
-list_of_eng= ('eng.10 Cylinders', 'eng.12 Cylinders',
-       'eng.3 Cylinders', 'eng.4 Cylinders', 'eng.5 Cylinders',
-       'eng.6 Cylinders', 'eng.8 Cylinders', 'eng.Other')
+list_of_eng= ('10 Cylinders', '12 Cylinders',
+       '3 Cylinders', '4 Cylinders', '5 Cylinders',
+       '6 Cylinders', '8 Cylinders', 'Other')
 eng= st.selectbox('Select The engine type on your car:', list_of_eng)
 #get user result
 states=[]
@@ -42,33 +35,33 @@ feul_types =[]
 engines=[]
 #engine
 for i in list_of_eng:
-       if i == eng:
+    if i == eng:
               engines.append(1)
-       else:
+    else:
               engines.append(0)
 #state
 for s in list_of_states:
-       if state == s:
+    if state == s:
               states.append(1)
-       else:
+    else:
               states.append(0)
 #manifacurers
 for m in list_of_manf:
-       if manf == m:
+    if manf == m:
               manfs.append(1)
-       else:
+    else:
               manfs.append(0)
 #transmissions
 for t in list_of_tran:
-       if Transmission == t:
+    if Transmission == t:
               tran.append(1)
-       else:
+    else:
               tran.append(0)
 #feul type
 for f in list_of_feul:
-       if feul == f:
+    if feul == f:
               feul_types.append(1)
-       else:
+    else:
               feul_types.append(0)
 
 year = st.text_input("Enter car's year of manufacture:  ", 0)
@@ -79,10 +72,10 @@ answers = np.concatenate([[year],[milage],manfs,feul_types,tran,engines,states])
 
 
 pred_btn = st.button('Predict Price')
-pickle_in = open('regressor.pkl', 'rb')
+pickle_in = open('regression/RegressionProject-main/regressor.pkl', 'rb')
 regressor = pickle.load(pickle_in)
 if pred_btn:
-       result= regressor.predict([answers])
-       result= result[0]
-       result = str(round(result, 2))
-       st.title(result)
+    result= regressor.predict([answers])
+    result= result[0]
+    result = str(round(result, 2))
+    st.title(result)
